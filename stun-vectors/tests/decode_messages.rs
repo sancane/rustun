b1259ef37cb79f7d63e03c709abafd35793b9e41
Santiago Carot-Nemesio <sancane@gmail.com>
2022-10-15 11:54:02 +0200

    stun-vectors: Add encode/decode tests

---
+++ stun-vectors/tests/decode_messages.rs
@@ -0,0 +1,237 @@
+use stun_rs::attributes::ice::{IceControlled, Priority};
+use stun_rs::attributes::stun::{
+    Fingerprint, MessageIntegrity, MessageIntegritySha256, Nonce, Realm, Software, UserHash,
+    UserName, XorMappedAddress,
+};
+use stun_rs::{
+    Algorithm, AlgorithmId, DecoderContextBuilder, HMACKey, MessageDecoderBuilder,
+    StunAttributeType,
+};
+
+// 2.1. Sample Request
+#[test]
+fn test_sample_request() {
+    let password = "VOkJxbRl1RmTxUk/WvJxBt";
+    let ctx = DecoderContextBuilder::default()
+        .with_key(HMACKey::new_short_term(password).expect("Can not create short term credential"))
+        .with_validation()
+        .build();
+    let decoder = MessageDecoderBuilder::default().with_context(ctx).build();
+
+    let (msg, size) = decoder
+        .decode(&stun_vectors::SAMPLE_REQUEST)
+        .expect("Can not decode StunMessage");
+    assert_eq!(size, 108);
+    let attributes = msg.attributes();
+    assert_eq!(attributes.len(), 6);
+
+    let mut iter = attributes.iter();
+    let attr = iter.next().expect("Expected attribute SOFTWARE");
+    let software = attr.expect_software();
+    assert_eq!(software.attribute_type(), Software::get_type());
+    assert_eq!(software, "STUN test client");
+
+    let attr = iter.next().expect("Expected attribute Priority");
+    let priority = attr.expect_priority();
+    assert_eq!(priority.attribute_type(), Priority::get_type());
+    assert_eq!(priority.as_u32(), 1845494271);
+
+    let attr = iter.next().expect("Expected attribute IceControlled");
+    let ice_controlled = attr.expect_ice_controlled();
+    assert_eq!(ice_controlled.attribute_type(), IceControlled::get_type());
+    assert_eq!(ice_controlled.as_u64(), 10605970187446795062);
+
+    let attr = iter.next().expect("Expected attribute Username");
+    let username = attr.expect_user_name();
+    assert_eq!(username.attribute_type(), UserName::get_type());
+    assert_eq!(username, "evtj:h6vY");
+
+    let attr = iter.next().expect("Expected attribute MessageIntegrity");
+    let integrity = attr.expect_message_integrity();
+    assert_eq!(integrity.attribute_type(), MessageIntegrity::get_type());
+
+    let attr = iter.next().expect("Expected attribute Fingerprint");
+    let fingerprint = attr.expect_fingerprint();
+    assert_eq!(fingerprint.attribute_type(), Fingerprint::get_type());
+}
+
+// 2.2. Sample IPv4 Response
+#[test]
+fn test_sample_ipv4_response() {
+    let password = "VOkJxbRl1RmTxUk/WvJxBt";
+    let ctx = DecoderContextBuilder::default()
+        .with_key(HMACKey::new_short_term(password).expect("Can not create short term credential"))
+        .with_validation()
+        .build();
+    let decoder = MessageDecoderBuilder::default().with_context(ctx).build();
+
+    let (msg, size) = decoder
+        .decode(&stun_vectors::SAMPLE_IPV4_RESPONSE)
+        .expect("Can not decode StunMessage");
+    assert_eq!(size, 80);
+    let attributes = msg.attributes();
+    assert_eq!(attributes.len(), 4);
+
+    let mut iter = attributes.iter();
+    let attr = iter.next().expect("Expected attribute SOFTWARE");
+    let software = attr.expect_software();
+    assert_eq!(software.attribute_type(), Software::get_type());
+    assert_eq!(software, "test vector");
+
+    let attr = iter.next().expect("Expected attribute XorMappedAddress");
+    let address = attr.expect_xor_mapped_address();
+    assert_eq!(address.attribute_type(), XorMappedAddress::get_type());
+    assert!(address.socket_address().is_ipv4());
+    assert_eq!(address.socket_address().to_string(), "192.0.2.1:32853");
+
+    let attr = iter.next().expect("Expected attribute MessageIntegrity");
+    let integrity = attr.expect_message_integrity();
+    assert_eq!(integrity.attribute_type(), MessageIntegrity::get_type());
+
+    let attr = iter.next().expect("Expected attribute Fingerprint");
+    let fingerprint = attr.expect_fingerprint();
+    assert_eq!(fingerprint.attribute_type(), Fingerprint::get_type());
+}
+
+// 2.3. Sample IPv6 Response
+#[test]
+fn test_sample_ipv6_response() {
+    let password = "VOkJxbRl1RmTxUk/WvJxBt";
+    let ctx = DecoderContextBuilder::default()
+        .with_key(HMACKey::new_short_term(password).expect("Can not create short term credential"))
+        .with_validation()
+        .build();
+    let decoder = MessageDecoderBuilder::default().with_context(ctx).build();
+
+    let (msg, size) = decoder
+        .decode(&stun_vectors::SAMPLE_IPV6_RESPONSE)
+        .expect("Can not decode StunMessage");
+    assert_eq!(size, 92);
+    let attributes = msg.attributes();
+    assert_eq!(attributes.len(), 4);
+
+    let mut iter = attributes.iter();
+    let attr = iter.next().expect("Expected attribute SOFTWARE");
+    let software = attr.expect_software();
+    assert_eq!(software.attribute_type(), Software::get_type());
+    assert_eq!(software, "test vector");
+
+    let attr = iter.next().expect("Expected attribute XorMappedAddress");
+    let address = attr.expect_xor_mapped_address();
+    assert_eq!(address.attribute_type(), XorMappedAddress::get_type());
+    assert!(address.socket_address().is_ipv6());
+    assert_eq!(
+        address.socket_address().to_string(),
+        "[2001:db8:1234:5678:11:2233:4455:6677]:32853"
+    );
+
+    let attr = iter.next().expect("Expected attribute MessageIntegrity");
+    let integrity = attr.expect_message_integrity();
+    assert_eq!(integrity.attribute_type(), MessageIntegrity::get_type());
+
+    let attr = iter.next().expect("Expected attribute Fingerprint");
+    let fingerprint = attr.expect_fingerprint();
+    assert_eq!(fingerprint.attribute_type(), Fingerprint::get_type());
+}
+
+// 2.4. Sample Request with Long-Term Authentication
+#[test]
+fn test_sample_request_with_long_term_auth() {
+    let username_str = "\u{30DE}\u{30C8}\u{30EA}\u{30C3}\u{30AF}\u{30B9}";
+    let realm_str = "example.org";
+    let algorithm = Algorithm::from(AlgorithmId::MD5);
+    let ctx = DecoderContextBuilder::default()
+        .with_key(
+            HMACKey::new_long_term(username_str, realm_str, "TheMatrIX", algorithm)
+                .expect("Can not create long term credential"),
+        )
+        .with_validation()
+        .build();
+    let decoder = MessageDecoderBuilder::default().with_context(ctx).build();
+
+    let (msg, size) = decoder
+        .decode(&stun_vectors::SAMPLE_REQUEST_LONG_TERM_AUTH)
+        .expect("Can not decode StunMessage");
+    assert_eq!(size, 116);
+    let attributes = msg.attributes();
+    assert_eq!(attributes.len(), 4);
+
+    let mut iter = attributes.iter();
+    let attr = iter.next().expect("Expected attribute USERNAME");
+    let username = attr.expect_user_name();
+    assert_eq!(attr.attribute_type(), UserName::get_type());
+    // Next comparation uses OpaqueString profile under the hood
+    assert_eq!(username, username_str);
+
+    let attr = iter.next().expect("Expected attribute Nonce");
+    let nonce = attr.expect_nonce();
+    assert_eq!(nonce.attribute_type(), Nonce::get_type());
+    assert_eq!(nonce, "f//499k954d6OL34oL9FSTvy64sA");
+
+    let attr = iter.next().expect("Expected attribute Realm");
+    let realm = attr.expect_realm();
+    assert_eq!(realm.attribute_type(), Realm::get_type());
+    // Next comparation uses OpaqueString profile under the hood
+    assert_eq!(realm, realm_str);
+
+    let integrity = iter.next().expect("Expected attribute MessageIntegrity");
+    assert_eq!(integrity.attribute_type(), MessageIntegrity::get_type());
+}
+
+// B.1.  Sample Request with Long-Term Authentication with MESSAGE-INTEGRITY-SHA256 and USERHASH.
+#[test]
+fn test_sample_request_with_long_term_auth_sha256() {
+    let username_str = "\u{30DE}\u{30C8}\u{30EA}\u{30C3}\u{30AF}\u{30B9}";
+    let realm_str = "example.org";
+    let algorithm = Algorithm::from(AlgorithmId::MD5);
+    let ctx = DecoderContextBuilder::default()
+        .with_key(
+            HMACKey::new_long_term(username_str, realm_str, "TheMatrIX", algorithm)
+                .expect("Can not create long term credential"),
+        )
+        .with_validation()
+        .build();
+    let decoder = MessageDecoderBuilder::default().with_context(ctx).build();
+
+    let (msg, size) = decoder
+        .decode(&stun_vectors::SAMPLE_REQUEST_LONG_TERM_AUTH_SHA256)
+        .expect("Can not decode StunMessage");
+    assert_eq!(size, 156);
+    let attributes = msg.attributes();
+    assert_eq!(attributes.len(), 4);
+
+    let mut iter = attributes.iter();
+    let attr = iter.next().expect("Expected attribute UserHash");
+    let userhash = attr.expect_user_hash();
+    assert_eq!(userhash.attribute_type(), UserHash::get_type());
+    let hash = [
+        0x4a, 0x3c, 0xf3, 0x8f, // }
+        0xef, 0x69, 0x92, 0xbd, // }
+        0xa9, 0x52, 0xc6, 0x78, // }
+        0x04, 0x17, 0xda, 0x0f, // }  `Userhash` value (32 bytes)
+        0x24, 0x81, 0x94, 0x15, // }
+        0x56, 0x9e, 0x60, 0xb2, // }
+        0x05, 0xc4, 0x6e, 0x41, // }
+        0x40, 0x7f, 0x17, 0x04, // }
+    ];
+    assert_eq!(userhash.hash(), hash);
+
+    let attr = iter.next().expect("Expected attribute Nonce");
+    let nonce = attr.expect_nonce();
+    assert_eq!(nonce.attribute_type(), Nonce::get_type());
+    assert_eq!(nonce, "obMatJos2AAACf//499k954d6OL34oL9FSTvy64sA");
+
+    let attr = iter.next().expect("Expected attribute Realm");
+    let realm = attr.expect_realm();
+    assert_eq!(realm.attribute_type(), Realm::get_type());
+    // Next comparation uses OpaqueString profile under the hood
+    assert_eq!(realm, realm_str);
+
+    let integrity = iter
+        .next()
+        .expect("Expected attribute MessageIntegritySha256");
+    assert_eq!(
+        integrity.attribute_type(),
+        MessageIntegritySha256::get_type()
+    );
+}
---
+++ stun-vectors/tests/encode_messages.rs
@@ -0,0 +1,226 @@
+use std::convert::TryFrom;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::str::FromStr;
+use stun_rs::attributes::ice::{IceControlled, Priority};
+use stun_rs::attributes::stun::{
+    Fingerprint, MessageIntegrity, MessageIntegritySha256, Nonce, Realm, Software, UserHash,
+    UserName, XorMappedAddress,
+};
+use stun_rs::methods::BINDING;
+use stun_rs::{
+    Algorithm, AlgorithmId, EncoderContextBuilder, HMACKey, MessageClass, MessageEncoderBuilder,
+    StunMessageBuilder, StunPadding, TransactionId,
+};
+use stun_vectors::{
+    SAMPLE_IPV4_RESPONSE, SAMPLE_IPV6_RESPONSE, SAMPLE_REQUEST, SAMPLE_REQUEST_LONG_TERM_AUTH,
+    SAMPLE_REQUEST_LONG_TERM_AUTH_SHA256,
+};
+
+// 2.1.  Sample Request
+#[test]
+fn test_sample_request() {
+    let transaction_id = TransactionId::from([
+        0xb7, 0xe7, 0xa7, 0x01, 0xbc, 0x34, 0xd6, 0x86, 0xfa, 0x87, 0xdf, 0xae,
+    ]);
+    let software = Software::new("STUN test client").expect("Could not create Software attribute");
+    let priority = Priority::from(0x6e0001ffu32);
+    let ice_controlled = IceControlled::from(0x932ff9b151263b36u64);
+    let user_name = UserName::try_from("evtj:h6vY").expect("Can not create USERNAME attribute");
+    let password = "VOkJxbRl1RmTxUk/WvJxBt";
+    let key = HMACKey::new_short_term(password).expect("Could not create HMACKey");
+    let integrity = MessageIntegrity::new(key);
+    let fingerprint = Fingerprint::default();
+
+    let msg = StunMessageBuilder::new(BINDING, MessageClass::Request)
+        .with_transaction_id(transaction_id)
+        .with_attribute(software)
+        .with_attribute(priority)
+        .with_attribute(ice_controlled)
+        .with_attribute(user_name)
+        .with_attribute(integrity)
+        .with_attribute(fingerprint)
+        .build();
+
+    let mut buffer: [u8; 150] = [0x00; 150];
+
+    // This example of the rfc5769 uses ASCII white spaces (U+0020)
+    // as padding which affects the message integrity fingerprint and CRC
+    let ctx = EncoderContextBuilder::default()
+        .with_custom_padding(StunPadding::Custom(0x20))
+        .build();
+
+    // Create a encoder that uses our custom context
+    let encoder = MessageEncoderBuilder::default().with_context(ctx).build();
+    let size = encoder
+        .encode(&mut buffer, &msg)
+        .expect("Could not encode STUN message");
+    assert_eq!(size, 108);
+
+    // Check that both vectors are equal
+    assert_eq!(buffer[..size], SAMPLE_REQUEST)
+}
+
+// 2.2.  Sample IPv4 Response
+#[test]
+fn test_sample_ipv4_response() {
+    let transaction_id = TransactionId::from([
+        0xb7, 0xe7, 0xa7, 0x01, 0xbc, 0x34, 0xd6, 0x86, 0xfa, 0x87, 0xdf, 0xae,
+    ]);
+    let software = Software::new("test vector").expect("Could not create Software attribute");
+    let mapped_address = XorMappedAddress::from(SocketAddr::new(
+        IpAddr::V4(Ipv4Addr::new(192, 0, 2, 1)),
+        32853,
+    ));
+    let password = "VOkJxbRl1RmTxUk/WvJxBt";
+    let key = HMACKey::new_short_term(password).expect("Could not create HMACKey");
+    let integrity = MessageIntegrity::new(key);
+    let fingerprint = Fingerprint::default();
+
+    let msg = StunMessageBuilder::new(BINDING, MessageClass::SuccessResponse)
+        .with_transaction_id(transaction_id)
+        .with_attribute(software)
+        .with_attribute(mapped_address)
+        .with_attribute(integrity)
+        .with_attribute(fingerprint)
+        .build();
+
+    let mut buffer: [u8; 80] = [0x00; 80];
+
+    // This example of the rfc5769 uses ASCII white spaces (U+0020)
+    // as padding which affects the message integrity fingerprint and CRC
+    let ctx = EncoderContextBuilder::default()
+        .with_custom_padding(StunPadding::Custom(0x20))
+        .build();
+
+    // Create a encoder that uses our custom context
+    let encoder = MessageEncoderBuilder::default().with_context(ctx).build();
+    let size = encoder
+        .encode(&mut buffer, &msg)
+        .expect("Could not encode STUN message");
+    assert_eq!(size, 80);
+
+    // Check that both vectors are equal
+    assert_eq!(buffer[..size], SAMPLE_IPV4_RESPONSE)
+}
+
+// 2.3.  Sample IPv6 Response
+#[test]
+fn test_sample_ipv6_response() {
+    let transaction_id = TransactionId::from([
+        0xb7, 0xe7, 0xa7, 0x01, 0xbc, 0x34, 0xd6, 0x86, 0xfa, 0x87, 0xdf, 0xae,
+    ]);
+    let software = Software::new("test vector").expect("Could not create Software attribute");
+    let addr = SocketAddr::from_str("[2001:db8:1234:5678:11:2233:4455:6677]:32853")
+        .expect("Can not parse SocketAddress");
+    let mapped_address = XorMappedAddress::from(addr);
+    let password = "VOkJxbRl1RmTxUk/WvJxBt";
+    let key = HMACKey::new_short_term(password).expect("Could not create HMACKey");
+    let integrity = MessageIntegrity::new(key);
+    let fingerprint = Fingerprint::default();
+
+    let msg = StunMessageBuilder::new(BINDING, MessageClass::SuccessResponse)
+        .with_transaction_id(transaction_id)
+        .with_attribute(software)
+        .with_attribute(mapped_address)
+        .with_attribute(integrity)
+        .with_attribute(fingerprint)
+        .build();
+
+    let mut buffer: [u8; 150] = [0x00; 150];
+
+    // This example of the rfc5769 uses ASCII white spaces (U+0020)
+    // as padding which affects the message integrity fingerprint and CRC
+    let ctx = EncoderContextBuilder::default()
+        .with_custom_padding(StunPadding::Custom(0x20))
+        .build();
+
+    // Create a encoder that uses our custom context
+    let encoder = MessageEncoderBuilder::default().with_context(ctx).build();
+    let size = encoder
+        .encode(&mut buffer, &msg)
+        .expect("Could not encode STUN message");
+    assert_eq!(size, 92);
+
+    // Check that both vectors are equal
+    assert_eq!(buffer[..size], SAMPLE_IPV6_RESPONSE)
+}
+
+// 2.4. Sample Request with Long-Term Authentication
+#[test]
+fn test_sample_request_with_long_term() {
+    let transaction_id = TransactionId::from([
+        0x78, 0xad, 0x34, 0x33, 0xc6, 0xad, 0x72, 0xc0, 0x29, 0xda, 0x41, 0x2e,
+    ]);
+    let username = UserName::try_from("\u{30DE}\u{30C8}\u{30EA}\u{30C3}\u{30AF}\u{30B9}")
+        .expect("Can not creat USERNAME attribute");
+    let nonce = Nonce::try_from("f//499k954d6OL34oL9FSTvy64sA").expect("Expected QuotedString");
+    let realm = Realm::try_from("example.org").expect("Expected QuotedString");
+    // Unicode codepoint {00AD} is disallowed in PRECIS, so we use the
+    // result of applying SASLprep
+    // let password = "The\u{00AD}M\u{00AA}tr\u{2168}";
+    let password = "TheMatrIX";
+    let algorithm = Algorithm::from(AlgorithmId::MD5);
+    let key = HMACKey::new_long_term(&username, &realm, password, algorithm)
+        .expect("Could not create HMACKey");
+    let integrity = MessageIntegrity::new(key);
+
+    let msg = StunMessageBuilder::new(BINDING, MessageClass::Request)
+        .with_transaction_id(transaction_id)
+        .with_attribute(username)
+        .with_attribute(nonce)
+        .with_attribute(realm)
+        .with_attribute(integrity)
+        .build();
+
+    let mut buffer: [u8; 150] = [0x00; 150];
+
+    let encoder = MessageEncoderBuilder::default().build();
+    let size = encoder
+        .encode(&mut buffer, &msg)
+        .expect("Could not encode STUN message");
+    assert_eq!(size, 116);
+
+    // Check that both vectors are equal
+    assert_eq!(buffer[..size], SAMPLE_REQUEST_LONG_TERM_AUTH)
+}
+
+// B.1.  Sample Request with Long-Term Authentication with MESSAGE-INTEGRITY-SHA256 and USERHASH.
+#[test]
+fn test_sample_request_with_long_term_sha256() {
+    let transaction_id = TransactionId::from([
+        0x78, 0xad, 0x34, 0x33, 0xc6, 0xad, 0x72, 0xc0, 0x29, 0xda, 0x41, 0x2e,
+    ]);
+    let username = UserName::try_from("\u{30DE}\u{30C8}\u{30EA}\u{30C3}\u{30AF}\u{30B9}")
+        .expect("Can not creat USERNAME attribute");
+    let realm = Realm::try_from("example.org").expect("Expected QuotedString");
+    let user_hash = UserHash::new(&username, &realm).expect("Can not create UserHash");
+    let nonce = Nonce::try_from("obMatJos2AAACf//499k954d6OL34oL9FSTvy64sA")
+        .expect("Expected QuotedString");
+    // Unicode codepoint {00AD} is disallowed in PRECIS, so we use the
+    // result of applying SASLprep
+    // let password = "The\u{00AD}M\u{00AA}tr\u{2168}";
+    let password = "TheMatrIX";
+    let algorithm = Algorithm::from(AlgorithmId::MD5);
+    let key = HMACKey::new_long_term(&username, &realm, password, algorithm)
+        .expect("Could not create HMACKey");
+    let integrity = MessageIntegritySha256::new(key);
+
+    let msg = StunMessageBuilder::new(BINDING, MessageClass::Request)
+        .with_transaction_id(transaction_id)
+        .with_attribute(user_hash)
+        .with_attribute(nonce)
+        .with_attribute(realm)
+        .with_attribute(integrity)
+        .build();
+
+    let mut buffer: [u8; 156] = [0x00; 156];
+
+    let encoder = MessageEncoderBuilder::default().build();
+    let size = encoder
+        .encode(&mut buffer, &msg)
+        .expect("Could not encode STUN message");
+    assert_eq!(size, 156);
+
+    // Check that both vectors are equal
+    assert_eq!(buffer[..size], SAMPLE_REQUEST_LONG_TERM_AUTH_SHA256)
+}
